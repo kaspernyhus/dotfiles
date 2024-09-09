4f6d49740e6ee8aa6beee444ba49f41aa80f75da
Kasper Nyhus <kanyhus@gmail.com>
2024-09-09 21:10:44 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,23 @@
 return {
   -- add gruvbox
   {
-    "ellisonleao/gruvbox.nvim",
-    config = true,
-    opts = {
-      terminal_colors = false,
-      bold = false,
-    },
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_palette = "material"
+      -- vim.cmd.colorscheme('gruvbox-material')
+    end,
   },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox-material",
     },
   },
 }
