852d9017faca8f64c092f5c1e4513ac111b8c0c3
Kasper Nyhus <kanyhus@gmail.com>
2025-05-13 20:51:22 +0200

    Remove .config/hypr/scripts/wallpaper/update_hyprlock.sh

--- dot_config/hypr/scripts/wallpaper/executable_update_hyprlock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wallpaper_path=$(hyprctl hyprpaper listloaded)
-orig_conf="$HOME/.config/hypr/hyprlock.conf"
-temp_conf="/tmp/hyprlock.conf"
-
-# Replace the path line
-sed -E "s|^\s*path = .*|    path = $wallpaper_path|" "$orig_conf" > "$temp_conf"
-
