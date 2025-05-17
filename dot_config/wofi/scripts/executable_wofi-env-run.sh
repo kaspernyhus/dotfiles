9bbcd36f549d34b24a16e7550b41ae9bb33ecd2a
Kasper Nyhus <kanyhus@gmail.com>
2025-05-17 07:19:39 +0200

    Update .config/wofi/scripts/wofi-env-run.sh

--- dot_config/wofi/scripts/executable_wofi-env-run.sh
+++ dot_config/wofi/scripts/executable_wofi-env-run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-MENU_CMD="wofi --show dmenu -i"
-ICONS_DIR="$HOME/.config/wofi/icons/"
+MENU_CMD="wofi --show dmenu -i -p hell -p 'Select dev env'"
+ACTION_DIR="$HOME/.config/wofi/actions"
 
-options=$(cat <<EOF
-    madplan
-EOF
-)
+# Build the options list by parsing .action files
+options=$(grep -h '^name=' "$ACTION_DIR"/*.action | cut -d= -f2-)
 
+# Let the user select
 selection=$(echo "$options" | eval "$MENU_CMD" | sed 's/^[[:space:]]*//')
 [ -z "$selection" ] && exit 0
 
-case "$selection" in
-    "madplan")
-        kitty --session /home/kaspernyhus/.config/kitty/sessions/eskemusro.session
-        ;;
-esac
+# Find the corresponding .entry file
+action_file=$(grep -l "name=$selection" "$ACTION_DIR"/*.action | head -n 1)
+[ -z "$action_file" ] && exit 1
+
+# Extract the action and execute it
+action=$(grep '^action=' "$action_file" | cut -d= -f2-)
+[ -n "$action" ] && eval "$action"
 
